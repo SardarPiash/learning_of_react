c7a8ea43a119170a5a5f082ae38cd7ff85af7fd6
Sardar Piash <piashm66@gmail.com>
2024-04-02 22:09:33 +0600

    Practise

--- Updating Arrays in State/src/App.jsx
+++ Updating Arrays in State/src/App.jsx
@@ -1,9 +1,50 @@
 import { useState } from "react";
 let id= 0;
+let initialShapes = [
+  { id: 0, type: 'circle', x: 50, y: 250 },
+  { id: 1, type: 'square', x: 150, y: 250 },
+  { id: 2, type: 'circle', x: 250, y: 250 },
+];
 export default function addElement() {
   const [name, setName] = useState("");
   const [array, setArray] = useState([]);
+  const [shapes,setShape] = useState(initialShapes);
 
+  function handleClick(){
+    setShape(
+      shapes.map((shape)=>{
+        if(shape.type === "circle"){
+          
+          return {
+            ...shape,
+            y:shape.y+50
+          }
+          
+        }else{
+          return shape;
+           
+        }
+      })
+    ) 
+    
+    ///////////orrrrrrrrrr
+    
+    //   const newShape=shapes.map((shape)=>{
+    //     if(shape.type === "circle"){
+          
+    //       return {
+    //         ...shape,
+    //         y:shape.y+50
+    //       }
+          
+    //     }else{
+    //       return shape;
+           
+    //     }
+    //   })
+    // setShape(newShape);
+    
+  }
 
   return (
     <div>
@@ -33,6 +74,29 @@ export default function addElement() {
                     <button onClick={()=>setArray(array.filter(a=> a.id!== item.id))}>Delete</button>
                     </li>
                 ))}
+
+
+
+                <div>
+                <button onClick={handleClick}>
+        Move circles down!
+      </button>
+      {shapes.map(shape => (
+        <div
+          key={shape.id}
+          style={{
+          background: 'purple',
+          position: 'absolute',
+          left: shape.x,
+          top: shape.y,
+          borderRadius:
+            shape.type === 'circle'
+              ? '50%' : '',
+          width: 20,
+          height: 20,
+        }} />
+      ))}
+                </div>
     </div>
   );
 }
