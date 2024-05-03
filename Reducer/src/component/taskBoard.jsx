eb2ce7b026455eeffd9e2a22a416fdcb8f9d37fa
Sardar Piash <piashm66@gmail.com>
2024-05-03 21:13:35 +0600

    Learning of reducer function

--- Reducer/src/component/taskBoard.jsx
+++ Reducer/src/component/taskBoard.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 import Addtask from "./addtask";
 import TaskList from "./TaskList";
 import { initialTasks } from "../data/data";
+import TaskReducer from "../reducer/taskReducer";
 
 export default function TaskBoard() {
-  const [tasks,setTasks]= useState(initialTasks);
+  const [tasks,dispatch]= useReducer(TaskReducer,initialTasks);
 
   ///calculate the nextID use reduce function......
   const nextID=(tasks)=>{
@@ -17,26 +18,45 @@ export default function TaskBoard() {
 
   ///handle add new task........
   function handleAddTask(text){
-    const addNewTask= [
-      ...tasks,
-      {
+    
+    dispatch({
+        type:"added",
         id:nextID(tasks),
-        text:text,
-        done:false
-      }
-    ]
-    setTasks(addNewTask);
+        text:text
+    })
+
+    // const addNewTask= [
+    //   ...tasks,
+    //   {
+    //     id:nextID(tasks),
+    //     text:text,
+    //     done:false
+    //   }
+    // ]
+    // setTasks(addNewTask);
   }
 
 
   function handleEditTask(Task){
-   const EditTask = (tasks.map(task=>task.id === Task.id ? Task : task))
-   setTasks(EditTask);
+    dispatch(
+      {
+        type:"edited",
+        task:Task, 
+      }
+    )
+  //  const EditTask = (tasks.map(task=>task.id === Task.id ? Task : task))
+  //  setTasks(EditTask);
   }
 
   function handleDelte(ID){
-   const afterDeletedTask= tasks.filter(task=> task.id !== ID)
-   setTasks(afterDeletedTask);
+    dispatch(
+      {
+        type:"deleted",
+        ID:ID,
+      }
+    )
+  //  const afterDeletedTask= tasks.filter(task=> task.id !== ID)
+  //  setTasks(afterDeletedTask);
   }
   return (
     <div>
