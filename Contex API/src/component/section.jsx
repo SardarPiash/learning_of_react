817a2edff6f94761300b97ea722717e03b643c6a
Sardar Piash <piashm66@gmail.com>
2024-05-05 15:21:34 +0600

    contex api

--- Contex API/src/component/section.jsx
+++ Contex API/src/component/section.jsx
@@ -1,14 +1,11 @@
 import { LevelContex } from "../contexAPI/LevelContex";
 
 export default function section({ level, children }) {
-    return (
-      <section style={{border:"2px solid black"}}>
-        <div style={{margin:"4px"}}>
-            <LevelContex.Provider value={level}>
-            {children}
-            </LevelContex.Provider>
-        </div>
-      </section>
-    );
-  }
-  
+  return (
+    <section style={{ border: "2px solid black" }}>
+      <div style={{ margin: "4px" }}>
+        <LevelContex.Provider value={level}>{children}</LevelContex.Provider>
+      </div>
+    </section>
+  );
+}
